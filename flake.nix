40ac73c0ab6f7038dc5dc12491d9e5c24a7d7619
Kat Inskip <kat@inskip.me>
2025-07-13 06:24:45 -0700

    feat: move to flakelib.std sort of o.o

--- flake.nix
+++ flake.nix
@@ -2,9 +2,15 @@
   description = "Tree, an import helper for Nix";
   inputs = {
     nixpkgs.url = "github:nix-community/nixpkgs.lib";
-    std.url = "github:chessai/nix-std";
+    nix-std.url = "github:chessai/nix-std";
+    std = {
+      url = "github:flakelib/std";
+      inputs = {
+        nix-std.follows = "nix-std";
+      };
+    };
   };
   outputs = {nixpkgs, std, ...}: {
-    tree = import ./tree.nix {inherit (nixpkgs) lib; std = std.lib;};
+    tree = import ./tree.nix {inherit (nixpkgs) lib; std = std.lib.Std.compat;};
   };
 }
